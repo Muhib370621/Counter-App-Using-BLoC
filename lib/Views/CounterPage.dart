30c30fd597b428243b8dcace1bf3b8b782c09914
Muhib370621 <marif@students.uit.edu>
2022-09-27 17:24:29 +0500

    final add

--- lib/Views/CounterPage.dart
+++ lib/Views/CounterPage.dart
@@ -10,26 +10,50 @@ class CounterPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
           title: Text(
-            "Bloc Counter",
-          )),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            ElevatedButton(onPressed: () {
-              BlocProvider.of<CounterCubit>(context).decrement();
-            }, child: Icon(Icons.arrow_back_ios)),
-            BlocBuilder<CounterCubit, CounterState>(
-              builder: (context, state) {
-                return Text(state.counterValue.toString(),style: TextStyle(fontSize: 30),);
-              },
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  BlocProvider.of<CounterCubit>(context).increment();
-                }, child: Icon(Icons.arrow_forward_ios)),
-          ],
+        "Bloc Counter",
+      )),
+      body: BlocListener<CounterCubit, CounterState>(
+        listener: (context, state) {
+          // TODO: implement listener
+          if (state.wasIncremented == true) {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text("Incremented"),
+              duration: Duration(milliseconds: 300),
+            ));
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text("Decremented"),
+                duration: Duration(milliseconds: 300),
+              ),
+            );
+          }
+        },
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).decrement();
+                  },
+                  child: Icon(Icons.arrow_back_ios)),
+              BlocBuilder<CounterCubit, CounterState>(
+                builder: (context, state) {
+                  return Text(
+                    state.counterValue.toString(),
+                    style: TextStyle(fontSize: 30),
+                  );
+                },
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).increment();
+                  },
+                  child: Icon(Icons.arrow_forward_ios)),
+            ],
+          ),
         ),
       ),
     );
--- lib/counter_state.dart
+++ lib/counter_state.dart
@@ -4,7 +4,9 @@ part of 'counter_cubit.dart';
 
 class  CounterState {
   int counterValue;
+  bool? wasIncremented;
   CounterState({
-    required this.counterValue
+    required this.counterValue,
+    this.wasIncremented,
   });
 }
